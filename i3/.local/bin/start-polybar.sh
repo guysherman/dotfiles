f36836e2b891d93709fc620ee020bf3f70c93626
Guy Sherman <guy@guysherman.com>
2022-02-22 21:13:05 +1300

    Fix polybar / i3-gaps so we can click on the tray

--- i3/.local/bin/start-polybar.sh
+++ i3/.local/bin/start-polybar.sh
@@ -3,7 +3,8 @@
 killall polybar
 
 echo "---" | tee -a /tmp/polybar1.log
-polybar dummy 2>&1 | tee -a /tmp/polybar-dummy1.log & disown
+#polybar dummy 2>&1 | tee -a /tmp/polybar-dummy1.log & disown
 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar tray 2>&1 | tee -a /tmp/polybar-tray1.log & disown
 
 sleep 3s
