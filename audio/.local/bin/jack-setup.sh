c41114ee8876750569716ca50b60b2eb44305c6d
Guy Sherman <guy@guysherman.com>
2022-08-20 15:00:48 +1200

    Move auto-disp into i3

--- audio/.local/bin/jack-setup.sh
+++ audio/.local/bin/jack-setup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+SAMPLE_RATE=44100
 
 CARLA_SESSION=/home/guy/.local/share/carla/BaseEnvironment.carxp
 jack_control ds alsa
@@ -7,19 +8,21 @@ if aplay -l | grep US4x4; then
 jack_control dps device hw:US4x4
 jack_control dps capture hw:US4x4
 jack_control dps playback hw:US4x4
+jack_control dps period 256
 CARLA_SESSION=/home/guy/.local/share/carla/SpeakerAndHeadphones.carxp 
 elif aplay -l | grep USB; then
 jack_control dps device hw:USB
 jack_control dps capture hw:USB
 jack_control dps playback hw:USB
+jack_control dps period 512
 else
 jack_control dps device hw:PCH
 jack_control dps capture hw:PCH
 jack_control dps playback hw:PCH
+jack_control dps period 1024
 fi
 
-jack_control dps rate 44100
-jack_control dps period 1024
+jack_control dps rate $SAMPLE_RATE
 jack_control dps nperiods 2
 jack_control dps hwmeter false
 jack_control dps duplex true
