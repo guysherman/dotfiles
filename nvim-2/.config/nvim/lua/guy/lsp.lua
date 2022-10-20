5a049f6cae1a771948f4e88cba6a9f1482bb06a7
Guy Sherman <guysnz@amazon.com>
2022-10-20 14:53:20 +1300

    Remove use of deprecated `update_capabilities` function

--- nvim-2/.config/nvim/lua/guy/lsp.lua
+++ nvim-2/.config/nvim/lua/guy/lsp.lua
@@ -121,7 +121,7 @@ else
   local opts = { noremap = true, silent = true }
   local function config(_config)
     return vim.tbl_deep_extend("force", {
-      capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
       on_attach = function(client, bufnr)
         -- Set custom stuff for some lanugage servers
         if client.name == "tsserver" then
