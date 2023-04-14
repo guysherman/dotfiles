b0ca4748aa0be9bd9206593d5d2e752e0a8fd754
Guy Sherman <guysnz@amazon.com>
2023-04-14 20:56:51 +1200

    Get ready to remove the shared "linux" pack

---
+++ desktop/.add-extra-modes.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+xrandr --newmode "2560x1080" 185.58 2560 2624 2688 2784 1080 1083 1093 1111 -hsync -vsync
+xrandr --addmode HDMI-1 "2560x1080"
+
+xrandr --newmode "3840x2160" 297.00 3840 4016 4104 4400 2160 2168 2178 2250 +hsync +vsync
+xrandr --addmode HDMI-1 "3840x2160"
+
---
+++ laptop/.config/.zshrc-platform.sh
@@ -0,0 +1,22 @@
+export KITTYMUX_STATE_DIR=$HOME/.local/state/kittymux
+
+# virtualenv and virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
+
+export GOPATH=/opt/go
+
+export PATH=$GOPATH/bin:$PATH
+
+# jEnv
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+# Maven
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH="$MAVEN_HOME/bin:$PATH"
+
+# Completion for kitty
+kitty + complete setup zsh | source /dev/stdin
