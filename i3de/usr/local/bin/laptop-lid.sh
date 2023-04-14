5d051bd80fa507c846c863d5fda9c6d1a72b70f4
Guy Sherman <guy@guysherman.com>
2023-04-14 22:52:38 +1200

    Handle multiple usernames and home display quirks

--- i3de/usr/local/bin/laptop-lid.sh
+++ i3de/usr/local/bin/laptop-lid.sh
@@ -6,10 +6,10 @@
 # udev runs as root, so we need to tell it how to connect to the X server:
 export DISPLAY=:0
 export XAUTHORITY=/run/user/1000/gdm/Xauthority
-export XDG_CONFIG_HOME=/home/ANT.AMAZON.COM/guysnz/.config
-export XDG_STATE_HOME=/home/ANT.AMAZON.COM/guysnz./.local/state
-export XDG_DATA_HOME=/home/ANT.AMAZON.COM/guysnz/.local/share
-export XDG_CACHE_HOME=/home/ANT.AMAZON.COM/guysnz/.cache
+export XDG_CONFIG_HOME=/home/ANT.AMAZON.COM/$user_name/.config
+export XDG_STATE_HOME=/home/ANT.AMAZON.COM/$user_name./.local/state
+export XDG_DATA_HOME=/home/ANT.AMAZON.COM/$user_name/.local/share
+export XDG_CACHE_HOME=/home/ANT.AMAZON.COM/$user_name/.cache
 export XDG_SEAT=seat0
 export XDG_SESSION_TYPE=x11
 export XDG_SESSION_CLASS=user
@@ -17,27 +17,37 @@ export XDG_RUNTIME_DIR=/run/user/1000
 export XDG_CURRENT_DESKTOP=i3
 export XDG_SESSION_DESKTOP=i3
 export XDG_CONFIG_DIRS=/etc/xdg/xdg-i3:/etc/xdg
-export XDG_DATA_DIRS=/usr/share/i3:/home/ANT.AMAZON.COM/guysnz/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop
+export XDG_DATA_DIRS=/usr/share/i3:/home/ANT.AMAZON.COM/$user_name/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop
 
 # Find out the device path to our graphics card:
 #cardPath=/sys/$(udevadm info -q path -n /dev/dri/card0)
+user_name=guy
+grep -q guy:x /etc/passwd
+if [[ $? == 1 ]]; then
+  user_name=$user_name
+fi
 
 # Count how many displays are connected
-num_displays=$(runuser -l guysnz -c 'DISPLAY=:0 xrandr' | grep -c ' connected')
+num_displays=$(runuser -l $user_name -c 'DISPLAY=:0 xrandr' | grep -c ' connected')
 
 # Determine lid state
-grep -q closed /proc/acpi/button/lid/LID/state
+lid_path=/proc/acpi/button/lid/LID/state
+if [[ ! -f $lid_path ]]; then
+  lid_path=/proc/acpi/button/lid/LID0/state
+fi
+
+grep -q closed $lid_path 
 lid=$?
 
-echo "Displays: $num_displays, Lid: $lid"
+echo "Displays: $num_displays, Lid: $lid; User: $user_name"
 
 # Update displays
-runuser -l guysnz -c 'DISPLAY=:0 auto-disp-user.sh'
+runuser -l $user_name -c 'DISPLAY=:0 auto-disp-user.sh'
 
 # If we have only the laptop display, and we closed the lid, suspend
 if [[ $num_displays == 1 ]] && [[ $lid = 0 ]]; then
-  runuser -l guysnz -c 'echo -ne "\007"'
-  runuser -l guysnz -c 'DISPLAY=:0 betterlockscreen -l blur' &
+  runuser -l $user_name -c 'echo -ne "\007"'
+  runuser -l $user_name -c 'DISPLAY=:0 betterlockscreen -l blur' &
   sleep 2
   systemctl suspend
 fi
